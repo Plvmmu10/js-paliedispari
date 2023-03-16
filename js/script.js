a76404269f911ca505a8edd8e6a233d68d8dded3
Plvmmu10 <gluca0260@gmail.com>
2023-03-16 14:56:50 +0100

    added checkPalindrome function

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@
 // consegna = Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+const word = document.getElementById('wordInput');
 
+const palBtn = document.getElementById('palBtn')
+
+palBtn.addEventListener('click', checkPalindrome);
+
+
+// function to check if a string is palindrome
+function checkPalindrome (){
+    
+    let wordValue = word.value;
+    let wordReversed = wordValue.split('').reverse().join('');
+
+    if (wordReversed === wordValue){
+        document.getElementById('palindromeResult').innerHTML = '<h3 class="text-success"><strong>Questa parola è un palindromo</strong></h3>'
+    }
+    else{
+        document.getElementById('palindromeResult').innerHTML = '<h3 class="text-danger"><strong>Questa parola non è un palindromo</strong></h3>'
+    }
+    
+}
